f8bfa93fe30348e16aef6e13d03e83e7f0c7d5e8
Mateusz Adamowski <m.adamowski@alx.pl>
2022-04-25 15:00:40 +0200

    rozwiązanie

---
+++ bash/20_zadanie.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+teraz=$(date +%Y)
+
+read -p "Podaj rok urodzenia: " rokur
+
+wiek=$((teraz - rokur))
+
+if [[ "$wiek" -ge 18 ]]
+then
+	echo "Jesteś pełnoletni"
+else
+	echo "Niepełnoletni."
+	zailelat=$((18 - wiek))
+	echo "Do zobaczenia za $zailelat lat"
+fi
+
+if [[ "$wiek" -ge 30 ]]
+then
+	echo "Możesz kandydować na senatora"
+else
+	echo "Nie możesz kandydować na senatora"
+	zailelat=$((30 - wiek))
+	echo "Do zobaczenia za $zailelat lat"
+fi
+
+if [[ "$wiek" -ge 35 ]]
+then
+	echo "Możesz kandydować na prezydenta"
+else
+	echo "Nie możesz"
+	zailelat=$((35 - wiek))
+	echo "Do zobaczenia za $zailelat lat"
+fi
+
