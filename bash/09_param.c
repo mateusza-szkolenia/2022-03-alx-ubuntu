2d83eb4445a0bb6347bcfd62d41226a59a3d12c7
Mateusz Adamowski <m.adamowski@alx.pl>
2022-04-25 11:49:30 +0200

    parametry

---
+++ bash/09_param.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("Liczba parametrów: %d\n", argc);
+	int i;
+	for (i=0; i<argc; i++) {
+		printf("Param %d: %s\n", i, argv[i]);
+	}
+}
+
--- bash/09_parametry.sh
+++ bash/09_parametry.sh
@@ -2,6 +2,7 @@
 
 echo "Liczba parametrów: $#"
 
+echo "Parametr 0: $0"
 echo "Parametr 1: $1"
 echo "Parametr 2: $2"
 echo "Parametr 3: $3"
