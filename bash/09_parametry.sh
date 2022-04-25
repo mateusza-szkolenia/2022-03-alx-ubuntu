eea1cf845ab13d97efe74a2c202518866336f86e
Mateusz Adamowski <m.adamowski@alx.pl>
2022-04-25 12:15:02 +0200

    więcej parametrów + shift

--- bash/09_parametry.sh
+++ bash/09_parametry.sh
@@ -3,8 +3,15 @@
 echo "Liczba parametrów: $#"
 
 echo "Parametr 0: $0"
-echo "Parametr 1: $1"
-echo "Parametr 2: $2"
+echo "Parametr 1: $1/$#"
+echo "Parametr 2: $2/$#"
 echo "Parametr 3: $3"
 echo "Parametr 4: $4"
+echo "Parametr 9: $9"
+
+shift
+echo "Parametr 10: $9/$#"
+
+shift
+echo "Parametr 11: $9/$#"
 
