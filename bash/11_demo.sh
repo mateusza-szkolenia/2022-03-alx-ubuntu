60401f4aaf4f14b9ea1e853e60cbce393d9accb5
Mateusz Adamowski <m.adamowski@alx.pl>
2022-04-25 12:10:54 +0200

    klamerki

---
+++ bash/11_demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./09_parametry.sh raport-{wiosna,lato,jesien,zima}.txt
+
+./09_parametry.sh raport-{2021,2022}-{wiosna,lato,jesien,zima}.txt
+
+./09_parametry.sh raport-{2000..2022}.txt
+
+./09_parametry.sh student-{000001..000147}.txt
+
