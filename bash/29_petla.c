f50f237e2799e49a21b2cbed47987d862213e157
Mateusz Adamowski <m.adamowski@alx.pl>
2022-04-25 16:37:43 +0200

    pętle

---
+++ bash/29_petla.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int i = 0;
+
+	do {
+		printf("Robię\n");
+	} while ( i > 0 );
+
+	printf("Koniec\n");
+}
---
+++ bash/29_petla_do_while.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+i=0
+while true
+do
+	echo "Robię"
+	[[ "$i" > 0 ]] || break
+done
+echo "Koniec"
