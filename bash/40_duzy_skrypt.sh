e04241190ba10a63b1f960028f16f0b269b18669
Mateusz Adamowski <m.adamowski@alx.pl>
2022-04-26 11:08:03 +0200

    cena złota

--- bash/40_duzy_skrypt.sh
+++ bash/40_duzy_skrypt.sh
@@ -5,11 +5,11 @@ set -e
 URL=http://api.nbp.pl/api/cenyzlota/today
 PLIK_TYMCZASOWY=$(mktemp)
 
-curl -s $URL > $PLIK_TYMCZASOWY
+curl -s $URL > "$PLIK_TYMCZASOWY"
 
-cena=$(./wyciagnij_cene $PLIK_TYMCZASOWY)
+cena=$(./wyciagnij_cene "$PLIK_TYMCZASOWY")
 
-echo Cena: $cena
+echo "Cena: $cena"
 
-rm $PLIK_TYMCZASOWY
+rm "$PLIK_TYMCZASOWY"
 
